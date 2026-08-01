6a1df53a0cc6189f11459c1a3df473c9cb2f7ff5
tshepangmosalaesi@gmail.com <tshepangmosalaesi@gmail.com>
2026-08-01 14:27:15 +0000

    Bright TV Loveable Invite Link

---
+++ 4. Project presentation or Visualisations/Loveable Invite Link.sql
@@ -0,0 +1,3 @@
+-- Databricks notebook source
+--Bright TV Loveavle Invite Link:
+https://lovable.dev/projects/e533a274-aa48-455f-abde-01c0dd1dae80?magic_link=mc_f06b347c-37ff-40bb-b662-b7b75c2ef967
